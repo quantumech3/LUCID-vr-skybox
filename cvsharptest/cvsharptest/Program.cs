924f76e36bb8abc0c39590c5e55ad40daab9a834
quantumech3 <quantumech3@gmail.com>
2022-06-02 13:34:47 -0400

    attempted to use OpenCV with C# and then Zhou came over

---
+++ cvsharptest/cvsharptest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+
+namespace cvsharptest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Cv2.GetVersionString());
+
+        }
+    }
+}
